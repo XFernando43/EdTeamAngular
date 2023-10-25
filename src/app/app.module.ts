c1eac0d661a3b5d3ec8778b629dcf737e2fd70cf
XFernando43 <U20171E997@upc.edu.pe>
2023-10-24 23:03:36 -0500

    add initial proyect

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TableInformationComponent } from './table-information/table-information.component';
+import { AuthComponent } from './modules/auth/auth.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableInformationComponent,
+    AuthComponent
   ],
   imports: [
     BrowserModule,
